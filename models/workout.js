87364425db1842904c37d09a43064fd2a5799329
Nigar Abdullayeva <nigar_abdullaeva@hotmail.com>
2020-09-01 20:27:39 -0500

    models folder

---
+++ models/workout.js
@@ -0,0 +1,20 @@
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  
+  //exercise 
+
+  
+  
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
